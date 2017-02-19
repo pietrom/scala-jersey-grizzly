3df3165543f2fe670f2f977df7280840f46179e3
Pietro Martinelli <amicofragile@gmail.com>
2017-02-19 16:04:53 +0100

    Switched secured GET /{id} api from Java to Scala

--- src/main/scala/com/example/rest/MyScalaResource.scala
+++ src/main/scala/com/example/rest/MyScalaResource.scala
@@ -7,6 +7,11 @@ import javax.ws.rs.Produces
 import javax.ws.rs.GET
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.QueryParam
+import javax.annotation.security.RolesAllowed
+import javax.ws.rs.core.SecurityContext
+import javax.ws.rs.PathParam
+import javax.ws.rs.core.Context
+
 
 @Path("myscalaresource")
 class MyScalaResource @Inject() (private val repo: PersonRepository) {
@@ -21,4 +26,17 @@ class MyScalaResource @Inject() (private val repo: PersonRepository) {
   @Path("add")
   @Produces(Array(MediaType.APPLICATION_JSON))
   def add(@QueryParam("x") x: Int, @QueryParam("y") y: Int) = x + y
+  
+  @GET
+  @Path("{id}")
+  @RolesAllowed(Array("standard"))
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def get(@PathParam("id") id: String, @Context sc: SecurityContext) = {
+    def f = (x: Person) => { x.lastName == id }
+  	val person:Option[Person] = repo.getAll().find(f)
+  	person match {
+  	  case None => throw new DataNotFoundException("Person not found: " + id)
+  	  case Some(p) => p
+  	}
+  }
 }
