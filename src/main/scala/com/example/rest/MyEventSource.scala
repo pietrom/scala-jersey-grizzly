bdf14c4ea8676e77036437837536904123878dcd
Pietro Martinelli <amicofragile@gmail.com>
2017-03-02 21:36:33 +0100

    EventSource with Server-Sent Events

---
+++ src/main/scala/com/example/rest/MyEventSource.scala
@@ -0,0 +1,43 @@
+package com.example.rest
+
+import javax.ws.rs._
+import javax.inject.Inject
+import org.glassfish.jersey.media.sse.SseFeature
+import org.glassfish.jersey.media.sse.EventOutput
+import org.glassfish.jersey.media.sse.OutboundEvent
+
+@Path("events")
+class MyEventSource @Inject() (private val repo: PersonRepository) {
+  @GET
+  @Produces(Array(SseFeature.SERVER_SENT_EVENTS))
+  def getEvents(): EventOutput = {
+    val eventOutput = new EventOutput()
+    new Thread(new Runnable {
+      override def run(): Unit = {
+        try {
+          for (i <- 1 to 10) {
+            Thread.sleep(250)
+            val eventBuilder = new OutboundEvent.Builder()
+            eventBuilder.name("message-to-client")
+            val msg = "Hello world " + i + "!"
+            eventBuilder.data(classOf[String], msg)
+            val event = eventBuilder.build()
+            eventOutput.write(event)
+            println("Message written " + msg)
+          }
+        } catch {
+          case e: Exception => e.printStackTrace()
+        } finally {
+          try {
+            println("Closing output")
+            eventOutput.close()
+          } catch {
+
+            case e: Exception => println("Error closing output")
+          }
+        }
+      }
+    }).start()
+    eventOutput
+  }
+}
---
+++ src/main/scala/com/example/rest/MyJavaEventSource.java
@@ -0,0 +1,49 @@
+package com.example.rest;
+
+import java.io.IOException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.glassfish.jersey.media.sse.EventOutput;
+import org.glassfish.jersey.media.sse.OutboundEvent;
+import org.glassfish.jersey.media.sse.SseFeature;
+
+@Path("jevents")
+public class MyJavaEventSource {
+	@GET
+    @Produces(SseFeature.SERVER_SENT_EVENTS)
+    public EventOutput getServerSentEvents() {
+        final EventOutput eventOutput = new EventOutput();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        // ... code that waits 1 second
+                        final OutboundEvent.Builder eventBuilder
+                        = new OutboundEvent.Builder();
+                        eventBuilder.name("message-to-client");
+                        String msg = "Hello world " + i + "!";
+						eventBuilder.data(String.class, msg);
+                        final OutboundEvent event = eventBuilder.build();
+                        eventOutput.write(event);
+                        System.out.println("Message sent " + msg);
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(
+                        "Error when writing the event.", e);
+                } finally {
+                    try {
+                        eventOutput.close();
+                    } catch (IOException ioClose) {
+                        throw new RuntimeException(
+                            "Error when closing the event output.", ioClose);
+                    }
+                }
+            }
+        }).start();
+        return eventOutput;
+    }
+}
